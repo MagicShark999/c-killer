974c5003698679637d5d77494667a75a3f9c0980
MagicShark999 <1106289640@qq.com>
2023-10-04 10:37:50 +0800

    计算偶数

--- calc_even_sum/index.c
+++ calc_even_sum/index.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
+
+/*
+计算1到x之间所有偶数的和
+
+@param {int} x   
+
+*/
 int calc_even_sum(int x){
-   // 请在此处完成代码
-   int i=0;
+    int i = 0;
+    int sum = 0;
+    for (i = 0; i <= x; i++) {
+        if (i % 2 == 0){
+            sum = sum + i;
+        }
+    }
+    return sum;
 }
 
-
-int main(int argc,char* argv[]){
-    // 请在此处完成代码
-
+int main(int argc, char *argv[]){
+    int b = 0;
+    printf("Please enter:");
+    scanf("%d", &b);
+    printf("1-%d的偶数之和为%d", b, calc_even_sum(b));
 }
