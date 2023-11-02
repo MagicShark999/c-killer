353ed3bfb027cf89eb7195e63b62eb5059ed823f
zhangfisher <wxzhang@126.com>
2023-11-02 11:41:30 +0800

    add 打印菱形图形

---
+++ 9.print_diamond.c
@@ -0,0 +1,40 @@
+/**
+ * 
+ * 输入一个菱形的边长正整数N,1<=N<=300
+ * 
+ * 输出菱形由大写字母组成,首字符为Z,依次Y,X,....A,Z,Y,X...A...
+ * 
+ * 例:
+ * 
+ * n=6
+ * 
+ * 输出:
+           Z
+         Y   X 
+       W        V
+     U            T
+   S                R
+ Q                    P
+   O                N
+     M            L
+        K       J
+          I   H
+            G
+ * 
+*/
+
+
+
+/**
+ * 输出n个空格
+ * 
+ * 例: output_backspace(5)代表输出5个空格
+ * 
+ */
+void output_backspace(int n){
+
+}
+
+int main(){
+
+}
