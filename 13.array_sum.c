27225c0d2cd1a6c6f3f711361631cb77c9af5531
zhangfisher <wxzhang@126.com>
2023-11-11 14:15:25 +0800

    add array_sum

---
+++ 13.array_sum.c
@@ -0,0 +1,42 @@
+/**
+ * 
+ * 编写一个函数用来计算数组元素之和
+ * 
+ * 要求:
+ * - 编写sum_array函数, 用来计算数组元素之和
+ * - 在main函数中
+ *   - 输入任意数字n(n>0 && n<1000),
+ *   - 调用上一个示例中编写的new_array函数, 生成长度为n的数组
+ *   - 将生成的数组传入sum_array函数, 计算数组元素之和并打印输出
+ *   {1,2,3,4,....,100} = 5050
+ * 
+ * 知识点:
+ * 
+ * 1. 如何调用保存的12.array_generate.c中编写的new_array函数
+ *    include "12.array_generate.c"        // 这样就可以调用12.array_generate.c中的函数了
+ *    int* p = new_array(100)   
+ * 2. 如何传入数组作为函数的参数
+ *    int sum_array(int arr[])
+ * 3. 如何得到数组的长度? sizeof(arr)/sizeof(arr[0])??
+ * 
+ *    
+ * 
+ * 
+*/
+
+
+
+/**
+ * 计算数组元素之和
+ * 
+ * arr: 数组
+ * 
+*/
+int sum_array(int arr[]){
+ 
+}
+
+
+void main(){
+ 
+}
